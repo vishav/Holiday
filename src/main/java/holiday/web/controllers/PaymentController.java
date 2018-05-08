830e1593d6a79995b479acfe3d22ddf354f4f2d2
vishav <vishavpandhi@gmail.com>
2018-05-07 20:05:25 -0400

    fixed error response handling in payment.

--- src/main/java/holiday/web/controllers/PaymentController.java
+++ src/main/java/holiday/web/controllers/PaymentController.java
@@ -18,13 +18,9 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    @Autowired
-    private OrderService orderService;
-
     @RequestMapping(method = RequestMethod.POST, value = "/payment")
     public PaymentResponse payment(@RequestBody Payment payment)
     {
-        System.out.println("userpayment:"+payment);
         PaymentResponse paymentResponse = paymentService.payment(payment);
 
         return paymentResponse;
@@ -33,48 +29,6 @@ public class PaymentController {
     @RequestMapping(method = RequestMethod.POST, value = "/refund")
     public RefundResponse refundPayment(@RequestBody Refund refund)
     {
-        RefundResponse refundResponse = new RefundResponse();
-
-        System.out.println("refund:"+refund);
-        CheckoutOrder checkoutOrder = orderService.getOrder(refund.getOrderId());
-
-        double originalRefundedAmount = checkoutOrder.getRefundAmount();
-
-        // if the order_id doesn't match with any existing order, it's an invalid order
-        if(checkoutOrder==null){
-            refundResponse.setSuccess(false);
-            refundResponse.setRefundMessage("Invalid request");
-            return refundResponse;
-        }
-
-        /* if the sum of amount to be refunded and the total
-         amount refunded till today > total order price, it's invalid*/
-        if(refund.getRefundAmount() <= 0 || (refund.getRefundAmount() + originalRefundedAmount) > checkoutOrder.getTotal()){
-            refundResponse.setSuccess(false);
-            refundResponse.setRefundMessage("Invalid refund amount value");
-            return refundResponse;
-        }
-
-        // get the payment id associated wih that order
-        String paymentId = checkoutOrder.getPaymentId();
-
-        // refund the amount which is asked
-        refundResponse = paymentService.refundPayment(refund.getRefundAmount(), paymentId);
-
-        // update the the total refunded amount till now
-        double totalRefundedAmount = originalRefundedAmount + refund.getRefundAmount();
-        checkoutOrder.setRefundAmount(totalRefundedAmount);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-        LocalDateTime currentDate = LocalDateTime.now();
-        // update the date when the last refund occurred
-        checkoutOrder.setRefundDate(currentDate);
-
-        // save the order
-        orderService.saveOrder(checkoutOrder);
-
-        refundResponse.setRefundAmount(totalRefundedAmount);
-        refundResponse.setRefundDate(currentDate.format(formatter).toString());
-        return refundResponse;
+        return paymentService.refundPayment(refund);
     }
 }
--- src/main/java/holiday/web/services/PaymentService.java
+++ src/main/java/holiday/web/services/PaymentService.java
@@ -5,16 +5,19 @@ import com.braintreegateway.Result;
 import com.braintreegateway.Transaction;
 import com.braintreegateway.TransactionRequest;
 import holiday.web.HolidayApplication;
-import holiday.web.entities.Payment;
-import holiday.web.entities.PaymentResponse;
-import holiday.web.entities.Refund;
-import holiday.web.entities.RefundResponse;
+import holiday.web.entities.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class PaymentService {
+
+    @Autowired
+    private OrderService orderService;
 
     private BraintreeGateway gateway = HolidayApplication.gateway;
 
@@ -42,7 +45,7 @@ public class PaymentService {
 
         if (result.isSuccess()) {
             Transaction transaction = result.getTarget();
-            paymentResponse.setTransactionID(transaction.getOrderId());
+            paymentResponse.setTransactionID(transaction.getId());
             paymentResponse.setSuccess(true);
         } else {
             paymentResponse.setSuccess(false);
@@ -51,16 +54,58 @@ public class PaymentService {
         return paymentResponse;
     }
 
-    public RefundResponse refundPayment(double refundAmount, String paymentId) {
+    public RefundResponse refundPayment(Refund refund) {
 
         RefundResponse refundResponse = new RefundResponse();
+
+        CheckoutOrder checkoutOrder = orderService.getOrder(refund.getOrderId());
+
+        // if the order_id doesn't match with any existing order, it's an invalid order
+        if(checkoutOrder==null){
+            refundResponse.setSuccess(false);
+            refundResponse.setRefundMessage("Invalid request");
+            return refundResponse;
+        }
+
+        double originalRefundedAmount = checkoutOrder.getRefundAmount();
+
+        /* if the sum of amount to be refunded and the total
+         amount refunded till today > total order price, it's invalid*/
+        if(refund.getRefundAmount() <= 0 || (refund.getRefundAmount() + originalRefundedAmount) > checkoutOrder.getTotal()){
+            refundResponse.setSuccess(false);
+            refundResponse.setRefundMessage("Invalid refund amount value");
+            return refundResponse;
+        }
+
+        // get the payment id associated wih that order
+        String paymentId = checkoutOrder.getPaymentId();
+
+        double refundAmount = refund.getRefundAmount();
 
         Result<Transaction> result = gateway.transaction().refund(paymentId, new BigDecimal(refundAmount));
 
         if (result.isSuccess()) {
+
+            // update the the total refunded amount till now
+            double totalRefundedAmount = originalRefundedAmount + refund.getRefundAmount();
+            checkoutOrder.setRefundAmount(totalRefundedAmount);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+            LocalDateTime currentDate = LocalDateTime.now();
+            // update the date when the last refund occurred
+            checkoutOrder.setRefundDate(currentDate);
+
+            // save the order
+            orderService.saveOrder(checkoutOrder);
+
+            refundResponse.setRefundAmount(totalRefundedAmount);
+            refundResponse.setRefundDate(currentDate.format(formatter).toString());
+
             refundResponse.setSuccess(true);
+            refundResponse.setRefundMessage("Refund initiated successfully");
         } else {
             refundResponse.setSuccess(false);
+            refundResponse.setRefundMessage("Error occured while refunding. Please try again in some time.");
         }
 
         return refundResponse;
